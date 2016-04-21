d72385077efdecab7a60060ae5210f9ada74d462
Yu-Hsin Hung <hungys@hotmail.com>
2016-04-22 00:52:29 +0800

    Add link type option

--- hw2/lib/hw2.ex
+++ hw2/lib/hw2.ex
@@ -1,19 +1,29 @@
 defmodule Hw2 do
-    def init(name) do
+    def init(name, link \\ "perfect") do
         case name do
             "p" ->
-                launch("p", "q")
+                launch("p", "q", link)
             "q" ->
-                launch("q", "p")
+                launch("q", "p", link)
             _ ->
                 IO.puts("Only p and q are supported!")
         end
     end
 
-    def launch(name, dest) do
+    def launch(name, dest, link) do
         listener = spawn(Hw2, :listen, [dest])
-        pl = spawn(PerfectLink, :init, [name, listener])
-        sender(pl, dest)
+        case link do
+            "perfect" ->
+                pl = spawn(PerfectLink, :init, [name, listener])
+                sender(pl, dest)
+            "stubborn" ->
+                sl = spawn(StubbornLink, :init, [name, listener])
+                sender(sl, dest)
+            "fairloss" ->
+                fl = spawn(FairlossLink, :init, [name, listener])
+                sender(fl, dest)
+            _ -> IO.puts("Unsupported link type: #{link}")
+        end
     end
 
     def listen(dest) do
@@ -23,15 +33,15 @@ defmodule Hw2 do
         listen(dest)
     end
 
-    def sender(pl, dest) do
+    def sender(link, dest) do
         msg = IO.gets("> ")
         msg = String.strip(msg)
         case msg do
             "connect" -> connect(dest)
             "disconnect" -> disconnect(dest)
-            _ -> send pl, {:send, dest, UUID.uuid4(), msg}
+            _ -> send link, {:send, dest, UUID.uuid4(), msg}
         end
-        sender(pl, dest)
+        sender(link, dest)
     end
 
     def connect(dest) do
