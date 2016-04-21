bf55b01a47f74fa1b0afb0d93cb58ead9544c3b7
Yu-Hsin Hung <hungys@hotmail.com>
2016-04-22 02:41:52 +0800

    Add experiment code

--- hw2/lib/hw2.ex
+++ hw2/lib/hw2.ex
@@ -1,17 +1,20 @@
 defmodule Hw2 do
-    def init(name, link \\ "perfect") do
+    @msgsize 4096
+    @msgcount 51200
+
+    def init(name, link \\ "perfect", mode \\ :normal) do
         case name do
             "p" ->
-                launch("p", "q", link)
+                launch("p", "q", link, mode)
             "q" ->
-                launch("q", "p", link)
+                launch("q", "p", link, mode)
             _ ->
                 IO.puts("Only p and q are supported!")
         end
     end
 
-    def launch(name, dest, link) do
-        listener = spawn(Hw2, :listen, [dest])
+    def launch(name, dest, link, mode) do
+        listener = spawn(Hw2, :listen, [dest, mode])
         case link do
             "perfect" ->
                 pl = spawn(PerfectLink, :init, [name, listener])
@@ -26,11 +29,24 @@ defmodule Hw2 do
         end
     end
 
-    def listen(dest) do
+    def listen(dest, :normal) do
         receive do
             {:deliver, _, _, msg} -> IO.puts("#{dest}: #{msg}")
         end
-        listen(dest)
+        listen(dest, :normal)
+    end
+
+    def listen(dest, :exp) do
+        receive do
+            {:deliver, _, _, :start_exp} ->
+                ts = :os.system_time(:milli_seconds)
+                IO.puts("Experiment starts at #{ts}")
+            {:deliver, _, _, :end_exp} ->
+                ts = :os.system_time(:milli_seconds)
+                IO.puts("Experiment ends at #{ts}")
+            # _ -> IO.write(".")
+        end
+        listen(dest, :exp)
     end
 
     def sender(link, dest) do
@@ -39,6 +55,9 @@ defmodule Hw2 do
         case msg do
             "connect" -> connect(dest)
             "disconnect" -> disconnect(dest)
+            "exp" ->
+                send link, {:send, dest, UUID.uuid4(), :start_exp}
+                exp(link, dest, generate_random_string(@msgsize), @msgcount)
             _ -> send link, {:send, dest, UUID.uuid4(), msg}
         end
         sender(link, dest)
@@ -56,5 +75,18 @@ defmodule Hw2 do
             :true -> IO.puts("disconnected")
             :false -> IO.puts("disconnect fail")
         end
+    end
+
+    def exp(link, dest, msg, 0) do
+        send link, {:send, dest, UUID.uuid4(), :end_exp}
+    end
+
+    def exp(link, dest, msg, cnt) do
+        send link, {:send, dest, UUID.uuid4(), msg}
+        exp(link, dest, msg, cnt - 1)
+    end
+
+    def generate_random_string(length) do
+        :crypto.strong_rand_bytes(length) |> Base.url_encode64 |> binary_part(0, length)
     end
 end
