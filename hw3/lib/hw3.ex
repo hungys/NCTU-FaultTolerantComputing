2401ccaf586f90b8235a6326570db9449ee04926
Yu-Hsin Hung <hungys@hotmail.com>
2016-05-14 21:58:31 +0800

    Add sample topologies

--- hw3/lib/hw3.ex
+++ hw3/lib/hw3.ex
@@ -25,6 +25,7 @@ defmodule Hw3 do
             "neighbor" -> neighbor(bcast, Enum.at(tokens, 1))
             "connect" -> connect(bcast, Enum.at(tokens, 1))
             "disconnect" -> disconnect(bcast, Enum.at(tokens, 1))
+            "topo" -> topo(bcast, Enum.at(tokens, 1))
             _ -> broadcast(bcast, msg)
         end
         node_loop(bcast)
@@ -32,12 +33,12 @@ defmodule Hw3 do
 
     def listen(delivered, validation \\ :false) do
         receive do
-            {:deliver, bid, bsrc, msg} ->
+            {:deliver, _, bsrc, msg} ->
                 IO.puts("#{bsrc}: #{msg}")
                 if validation == :true do
                     delivered = validate_reliable(delivered, bsrc, msg)
                 end
-            {:deliver, bid, bsrc, seqno, msg} ->
+            {:deliver, _, bsrc, seqno, msg} ->
                 IO.puts("#{bsrc}: (seqno=#{seqno}) #{msg}")
                 if validation == :true do
                     delivered = validate_fifo(delivered, bsrc, msg)
@@ -55,7 +56,7 @@ defmodule Hw3 do
         node = String.to_atom("#{name}@localhost")
         send bcast, {:link, node}
         if Node.connect(node) do
-            IO.puts("Connect success.")
+            IO.puts("Connected to #{name}.")
         end
     end
 
@@ -63,7 +64,7 @@ defmodule Hw3 do
         node = String.to_atom("#{name}@localhost")
         send bcast, {:unlink, node}
         if Node.disconnect(node) do
-            IO.puts("Disconnect success.")
+            IO.puts("Disconnectted from #{name}.")
         end
     end
 
@@ -95,5 +96,92 @@ defmodule Hw3 do
 
     def md5(data) do
         Base.encode16(:erlang.md5(data), case: :lower)
+    end
+
+    def topo(bcast, id) do
+        name = Atom.to_string(Node.self)
+        name = Enum.at(String.split(name, "@"), 0)
+
+        case id do
+            "full3" ->
+                nodes = ["a", "b", "c"]
+                Enum.each(nodes, fn(n) ->
+                    if n != name do
+                        neighbor(bcast, n)
+                    end
+                end)
+            "full4" ->
+                nodes = ["a", "b", "c", "d"]
+                Enum.each(nodes, fn(n) ->
+                    if n != name do
+                        neighbor(bcast, n)
+                    end
+                end)
+            "full5" ->
+                nodes = ["a", "b", "c", "d", "e"]
+                Enum.each(nodes, fn(n) ->
+                    if n != name do
+                        neighbor(bcast, n)
+                    end
+                end)
+            "ring3" ->
+                case name do
+                    "a" ->
+                        neighbor(bcast, "b")
+                        neighbor(bcast, "c")
+                    "b" ->
+                        neighbor(bcast, "a")
+                        neighbor(bcast, "c")
+                    "c" ->
+                        neighbor(bcast, "a")
+                        neighbor(bcast, "b")
+                end
+            "ring4" ->
+                case name do
+                    "a" ->
+                        neighbor(bcast, "b")
+                        neighbor(bcast, "c")
+                    "b" ->
+                        neighbor(bcast, "a")
+                        neighbor(bcast, "d")
+                    "c" ->
+                        neighbor(bcast, "a")
+                        neighbor(bcast, "d")
+                    "d" ->
+                        neighbor(bcast, "b")
+                        neighbor(bcast, "c")
+                end
+            "ring5" ->
+                case name do
+                    "a" ->
+                        neighbor(bcast, "b")
+                        neighbor(bcast, "e")
+                    "b" ->
+                        neighbor(bcast, "a")
+                        neighbor(bcast, "c")
+                    "c" ->
+                        neighbor(bcast, "b")
+                        neighbor(bcast, "d")
+                    "d" ->
+                        neighbor(bcast, "c")
+                        neighbor(bcast, "e")
+                    "e" ->
+                        neighbor(bcast, "a")
+                        neighbor(bcast, "d")
+                end
+            "star4" ->
+                case name do
+                    "a" ->
+                        neighbor(bcast, "b")
+                        neighbor(bcast, "c")
+                        neighbor(bcast, "d")
+                    "b" ->
+                        neighbor(bcast, "a")
+                    "c" ->
+                        neighbor(bcast, "a")
+                    "d" ->
+                        neighbor(bcast, "a")
+                end
+        end
     end
 end
